311dacebd3f84b2f7186bf9179c17808d18c2338
silassentinel <53788992+Silassentinel@users.noreply.github.com>
2023-09-20 17:26:07 +0200

    merge

---
+++ src/scripts/Stylez.ts
@@ -0,0 +1,18 @@
+export const Stylez = {
+    /*
+            h1 {
+          color: purple;
+          font-size: 4rem;
+        }
+        .skill {
+          color: green;
+          font-weight: bold;
+    */
+};
+
+
+export const skillColor = [0, 255, 0];
+
+export const fontWeight: string = "bold";
+
+export const textCase: string = "2rem";
