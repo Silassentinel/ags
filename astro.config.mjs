a411bb9973217c6f9c40556fdaaef7d10b2e8f97
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2023-09-22 23:26:54 +0200

    6.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astrogettingstarted.netlify.app/'
+  site: 'https://astrogettingstarted.netlify.app/',
+  integrations: [preact()]
 });
