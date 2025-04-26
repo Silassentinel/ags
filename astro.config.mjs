c9bb1875302aef29804d5bbfe2b6a8528847c0f6
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2025-04-26 08:51:09 +0200

    baup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrogettingstarted.netlify.app/',
-  integrations: [preact()],
+  integrations: [preact(), compress()],
   ourDir: "../silassentinel.github.io/"
 });
